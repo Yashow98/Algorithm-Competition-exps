30afd7919a48d39b48f7a3602052a7ba85cf9e63
Yashow98 <yashowhoo@outlook.com>
2023-06-02 09:56:41 +0800

    Modify file.

--- chapter07/fp.cpp
+++ chapter07/fp.cpp
@@ -2,12 +2,14 @@
 
 int main()
 {
-    float a = -1.5e38, b = 1.5e38, c = 1.0;
-    float d = a + (b + c);
-    float e = a + b + c;
+    // float a = -1.5e38, b = 1.5e38, c = 1.0;
+    // float d = a + (b + c);
+    // float e = a + b + c;
 
-    printf("%f\n", d); // 0.000000
-    printf("%f\n", e); // 1.000000
+    // printf("%f\n", d); // 0.000000
+    // printf("%f\n", e); // 1.000000
     
+    float i = 1.5;
+    printf("%d\n", (int)i);
     return 0;
 }
