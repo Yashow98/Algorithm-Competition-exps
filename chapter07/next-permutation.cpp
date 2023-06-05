03590d1a417f007503f979ece65d7e4e98cf6826
Yashow98 <yashowhoo@outlook.com>
2023-06-05 23:30:23 +0800

    Modify file.

--- chapter07/next-permutation.cpp
+++ chapter07/next-permutation.cpp
@@ -1,16 +1,43 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <functional>
 
-void next_permutation(std::vector<int>& arr)
+// void next_permutation(std::vector<int>& arr)
+// {
+//     std::next_permutation(arr.begin(), arr.end());
+
+// }
+
+template<typename BidiIT>
+void nextPermutation(BidiIT first, BidiIT last)
 {
-    std::next_permutation(arr.begin(), arr.end());
+    const auto rfirst = std::reverse_iterator<BidiIT>(last);
+    const auto rlast = std::reverse_iterator<BidiIT>(first);
 
+    auto pivot = std::next(rfirst);
+
+    while (pivot != rlast && *pivot >= *std::prev(pivot))
+    {
+        ++pivot; 
+    }
+
+    if (pivot == rlast)
+    {
+        std::reverse(rfirst, rlast);
+    }
+
+    auto change_number = std::find_if(rfirst, pivot, std::bind(std::less<int>(), *pivot, std::placeholders::_1));
+
+    std::swap(*pivot, *change_number);
+    std::reverse(rfirst, pivot);
 }
+
 int main()
 {
     std::vector<int> arr = {1, 2, 3, 6, 5, 4};
-    next_permutation(arr);
+    // next_permutation(arr);
+    nextPermutation(arr.begin(), arr.end());
 
     for (auto i : arr)
     {
